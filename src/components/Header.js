38b47fd2b195ba2f868ef1c9d24edc882d3ffdb0
Peter Bonner <Peter.Bonner@fenergo.com>
2022-04-04 17:55:28 +0100

    React Hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,47 @@
-import React from "react";
-import menuLinksData from './data/menu_links.json';
+import React, { useState, useEffect } from 'react';
+// import menuLinksData from './data/menu_links.json'
 
 const Header = () => {
-    return (
+  const [menuLinksData, setMenuLinksData] = useState([]);
 
-<header id="intro">
-          <article className="fullheight">
-            <div className="hgroup">
-              <h1>Landon Hotel</h1>
-              <h2>West London</h2>
-              <p>
-                <a href="#welcome">
-                  <img
-                    src="https://landonhotel.com/images/misc/arrow.png"
-                    alt="down arrow"
-                  />
-                </a>
-              </p>
-            </div>
-          </article>
+  const loadMenuLinksData = async() => {
+    // Query the API Gateway
+    const resp = await fetch('https://eljuu3mlbi.execute-api.eu-west-1.amazonaws.com/menuLinks');
+    let jsonData = await resp.json();
 
-          <nav id="nav">
-            <div className="navbar">
-              <div className="brand">
-                <a href="#welcome">
-                  Landon <span>Hotel</span>
-                </a>
-              </div>
-              <ul>
-              {
+    // Assign response data to our state variable
+    setMenuLinksData(jsonData);
+  }
+
+  useEffect(() => {
+    // Load the menu links data from the API Gateway
+    loadMenuLinksData();
+  }, []);
+
+  return (
+    <header id="intro">
+      <article className="fullheight">
+        <div className="hgroup">
+          <h1>Landon Hotel</h1>
+          <h2>West London</h2>
+          <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+        </div>
+      </article>
+
+      <nav id="nav">
+        <div className="navbar">
+          <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+          <ul>
+            {
               menuLinksData.map((link) =>
                 <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
               )
             }
-               
-              </ul>
-            </div>
-          </nav>
-        </header>
-
-    );
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
+
 export default Header;
